10e0c0f140e610f223a9dabf4e86a513e80f44db
Hwangsedong <saedongh5@naver.com>
2026-02-14 15:15:21 +0900

    [Init] Initial commit

---
+++ src/test/java/com/gifo/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gifo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
