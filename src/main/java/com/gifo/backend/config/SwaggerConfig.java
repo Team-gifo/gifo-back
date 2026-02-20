fce13e407d42704cf33885321c88556d5fe419e9
saaad99 <ehdgnstla@naver.com>
2026-02-20 18:13:08 +0900

    ref: 사용하지 않는 import 제거

--- src/main/java/com/gifo/backend/config/SwaggerConfig.java
+++ src/main/java/com/gifo/backend/config/SwaggerConfig.java
@@ -2,10 +2,8 @@ package com.gifo.backend.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
