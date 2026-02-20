ca08c6517c53fc8da9417c99695c3d0a664e0b46
saaad99 <ehdgnstla@naver.com>
2026-02-20 18:10:01 +0900

    chore: 예외 관련 객체 생성(ErrorCode, GlobalException 등)

---
+++ src/main/java/com/gifo/backend/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,128 @@
+package com.gifo.backend.global.exception;
+
+import com.gifo.backend.global.ErrorCode;
+import com.gifo.backend.global.ErrorResponse;
+import jakarta.persistence.NoResultException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // 커스텀 처리
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustom(CustomException ex) {
+        log.error("[{}] Status: {}, CustomCode: {}, Message: {}",
+                ex.getClass().getSimpleName(),
+                ex.getStatus().value(),
+                ex.getCode(),
+                ex.getMessage());
+
+        return ResponseEntity
+                .status(ex.getStatus())
+                .body(ErrorResponse.fail(ex.getCode(), ex.getMessage()));
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String errorMessage = e.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.fail(ErrorCode.VALIDATION_ERROR, errorMessage));
+    }
+
+    // 기타 예외 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex) {
+        String message = "요청 url을 다시 확인해보세요 : " + ex.getMessage();
+        log.error(message, ex);
+        return ResponseEntity
+                .status(ErrorCode.UNKNOWN_REQUEST.getHttpStatus())
+                .body(ErrorResponse.fail(ErrorCode.UNKNOWN_REQUEST, message));
+    }
+
+    // 이메일 중복 인증 발생 예외 처리
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalStateException(IllegalStateException ex) {
+        log.error(ex.getMessage(), ex);
+        return ResponseEntity
+                .status(ErrorCode.ILLEGAL_STATE.getHttpStatus())
+                .body(ErrorResponse.fail(ErrorCode.ILLEGAL_STATE));
+    }
+
+    // 파라미터로 받은 값의 인자가 맞지 않을 때
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+        String message = String.format("잘못된 요청: '%s' 값을 '%s' 타입으로 변환할 수 없습니다.", ex.getValue(), ex.getRequiredType().getSimpleName());
+        log.error(message, ex);
+        return ResponseEntity.status(ErrorCode.TYPE_MISMATCH
+                        .getHttpStatus())
+                .body(ErrorResponse.fail(ErrorCode.TYPE_MISMATCH, message));
+    }
+
+    // 숫자 변환 실패
+    @ExceptionHandler(NumberFormatException.class)
+    public ResponseEntity<ErrorResponse> handleNumberFormatException(NumberFormatException ex) {
+        String message = "숫자 변환 오류: " + ex.getMessage();
+        log.error(message, ex);
+        return ResponseEntity.status(ErrorCode.NUMBER_FORMAT_ERROR.getHttpStatus()).body(ErrorResponse.fail(ErrorCode.NUMBER_FORMAT_ERROR, message));
+    }
+
+    // 데이터베이스 접근 오류
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<ErrorResponse> handleDataAccessException(DataAccessException ex) {
+        String message = "데이터베이스 오류 발생: " + ex.getMessage();
+        log.error(message, ex);
+        return ResponseEntity
+                .status(ErrorCode.DB_ACCESS_ERROR.getHttpStatus())
+                .body(ErrorResponse.fail(ErrorCode.DB_ACCESS_ERROR, message));
+
+    }
+
+    // 요구되는 값이 비어있을 때
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<ErrorResponse> handleNullPointerException(NullPointerException ex) {
+        String message = "필수 데이터가 누락되었습니다 : " + ex.getMessage();
+        log.error(ex.getMessage(), ex);
+        return ResponseEntity
+                .status(ErrorCode.NULL_POINTER.getHttpStatus())
+                .body(ErrorResponse.fail(ErrorCode.NULL_POINTER, message));
+
+    }
+
+    // 유효하지 않은 요청 파라미터
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        String message = "잘못된 요청: " + ex.getMessage();
+        log.error(message, ex);
+        return ResponseEntity
+                .status(ErrorCode.INVALID_ARGUMENT.getHttpStatus())
+                .body(ErrorResponse.fail(ErrorCode.INVALID_ARGUMENT, message));
+    }
+
+    // 특정 예외를 명확히 처리
+    @ExceptionHandler(NoResultException.class)
+    public ResponseEntity<ErrorResponse> handleNoResultException(NoResultException ex) {
+        log.error(ex.getMessage(), ex);
+        return ResponseEntity
+                .status(ErrorCode.NO_RESULT.getHttpStatus())
+                .body(ErrorResponse.fail(ErrorCode.NO_RESULT));
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<ErrorResponse> handleEmptyResultException(EmptyResultDataAccessException ex) {
+        String message = "결과가 존재하지 않습니다 : " + ex.getMessage();
+        log.error(message, ex);
+        return ResponseEntity
+                .status(ErrorCode.EMPTY_RESULT.getHttpStatus())
+                .body(ErrorResponse.fail(ErrorCode.EMPTY_RESULT, message));
+    }
+
+
+}
