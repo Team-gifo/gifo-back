4d88526487b7a1dac0209a2cf8ec871abca856b4
Jangdol <vkdnjdjxor@naver.com>
2026-04-13 09:41:45 +0900

    fix: @PostConstruct 대신 static 블록으로 타임존 설정 시점 앞당기기

--- src/main/java/com/gifo/backend/BackendApplication.java
+++ src/main/java/com/gifo/backend/BackendApplication.java
@@ -1,6 +1,5 @@
 package com.gifo.backend;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +8,7 @@ import java.util.TimeZone;
 @SpringBootApplication
 public class BackendApplication {
 
-	@PostConstruct
-	void setTimeZone() {
+	static {
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
